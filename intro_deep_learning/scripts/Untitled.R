5dd0e27880594ee426657a25d1d56dfa60d345e9
Cristian Pachón <cc.pachon@gmail.com>
2020-02-05 17:14:48 +0100

    Last commit

--- intro_deep_learning/scripts/Untitled.R
+++ intro_deep_learning/scripts/Untitled.R
@@ -15,6 +15,7 @@ df$pred =  4.0570 + 0.3769*df$height
 df$error = (df$pred - df$weight)^2
 sqrt(1/200*sum(df$error))
 epsilon = 10^(-6)
+
 x0 = 1 
 are_close = FALSE
 lr = 0.1
@@ -38,5 +39,50 @@ while(i <= 100 && !are_close){
 }
 
 xtable(df2)
+
+
+pol = function(x){
+  return(x^4 + x^3 -3*x^2 - 2*x + 2)
+}
+
+der = function(x){
+  return(4*x^3 + 3*x^2 - 6* x - 2)
+}
+
+x = seq(-2, 2, 0.0001)
+y = pol(x)
+
+df = data.frame(x, y)
+plot(x, y, type = 'l')
+
+x0 = 2
+are_close = FALSE
+lr = 0.001
+i = 1
+fx0 = pol(x0)
+df2 = data.frame(x0 = x0, fx0 = fx0, derivative = NA, x1 = NA, fx1 = NA, error = NA)
+while(i <= 500 && !are_close){
+  
+  derivative = der(x0)
+  x1 = x0 - lr*derivative
+  fx1 = pol(x1)
+  error = abs(fx1 - fx0)
+  are_close = error < epsilon
+  
+  df2 = rbind(df2, data.frame(x0 = x0, fx0 = fx0,derivative = derivative,
+                              x1 = x1,   fx1 = fx1, error = error))
+  
+  x0 = x1
+  fx0 = fx1
+  i = i +1 
+}
+
+tail(df2)
+
+xtable(df2, digits= 5)
+
+
+
+
 
 
